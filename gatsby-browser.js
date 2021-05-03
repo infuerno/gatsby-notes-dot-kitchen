3be770878e8ea2f446259be92f6c7e5afb165cd5
Claire Furney <infuerno@fastmail.fm>
2021-05-03 23:01:19 +0100

    Add prettier

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,11 @@
-import React from 'react';
-import Layout from './src/components/Layout';
+import React from "react";
+import Layout from "./src/components/Layout";
 
-export const wrapPageElement = ({ element, props }) => <><Layout {...props}>{element}</Layout></>;
+export const wrapPageElement = ({ element, props }) => (
+  <>
+    <Layout {...props}>{element}</Layout>
+  </>
+);
 
 // while we only have one export in this file
 export const foo = () => {};
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,8 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => (
   <footer>
-    <p>
-      &copy; notes.dot.kitchen
-      {' '}
-      {new Date().getFullYear()}
-    </p>
+    <p>&copy; notes.dot.kitchen {new Date().getFullYear()}</p>
   </footer>
 );
 
